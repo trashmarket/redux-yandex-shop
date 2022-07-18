8d794f4406518e66b3408ad130be6a62648a67e3
Nikita <prey122@gmail.com>
2022-07-18 23:04:58 +0300

    error

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+// Импортируем пакет path
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, './public'),
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true
+  },
+}; 
